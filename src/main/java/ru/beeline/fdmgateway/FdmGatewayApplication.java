44794cd86263bec805bc9d1548aa44b24caf2425
Зайнагтдинова Галия Нафисовна <GZaynagtdinova@beeline.ru>
2024-01-24 12:43:29 +0300

    SFDM-1277: создание репозитория под API Gateway

---
+++ src/main/java/ru/beeline/fdmgateway/FdmGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.beeline.fdmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdmGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdmGatewayApplication.class, args);
+    }
+
+}
