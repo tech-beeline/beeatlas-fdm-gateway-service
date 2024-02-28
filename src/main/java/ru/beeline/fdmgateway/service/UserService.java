28131a162d72d15830f91c8c3a0acf0028dff298
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2024-02-28 17:31:05 +0300

    SFDM-1384 fix config

--- src/main/java/ru/beeline/fdmgateway/service/UserService.java
+++ src/main/java/ru/beeline/fdmgateway/service/UserService.java
@@ -1,6 +1,8 @@
 package ru.beeline.fdmgateway.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.beeline.fdmgateway.dto.UserInfoDTO;
@@ -17,9 +19,10 @@ public class UserService {
     }
 
     public UserInfoDTO getUserInfo(String email, String fullName, String idExt) {
-        String login = email.substring(0, email.indexOf(","));
+        String login = email.substring(0, email.indexOf("@"));
         return webClient.get()
-                .uri(userServerUrl + "/api/admin/v1/user/" + login + "/info?&email=" + email + "&fullname=" + fullName + "&idExt=" + idExt)
+                .uri(userServerUrl + "/api/admin/v1/user/" + login + "/info?&email=" + email + "&fullName=" + fullName + "&idExt=" + idExt)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .bodyToMono(UserInfoDTO.class)
                 .block();
