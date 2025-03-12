7e4571712d01dbcc29f20c042636f1295d9e2cf1
salikhanov <SAlikhanov@oms.beeline.ru>
2025-03-12 21:48:14 +0600

    SFDM-2434 добавляем заголовок в ответ

--- src/main/java/ru/beeline/fdmgateway/config/CustomHeaderFilter.java
+++ src/main/java/ru/beeline/fdmgateway/config/CustomHeaderFilter.java
@@ -3,21 +3,31 @@ package ru.beeline.fdmgateway.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Slf4j
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Component
 public class CustomHeaderFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
             ServerHttpResponse response = exchange.getResponse();
-            response.getHeaders().add("Access-Control-Allow-Headers", "Content-Disposition");
-            response.getHeaders().forEach((name, values) -> {
-                log.info("Header: {} = {}", name, values);
+            HttpHeaders headers = response.getHeaders();
+            headers.compute("Access-Control-Allow-Headers", (key, existingValues) -> {
+                if (existingValues == null) {
+                    return List.of("Content-Type", "Authorization", "Content-Disposition");
+                }
+                Set<String> updatedValues = new HashSet<>(existingValues);
+                updatedValues.add("Content-Disposition");
+                return new ArrayList<>(updatedValues);
             });
         }));
     }
