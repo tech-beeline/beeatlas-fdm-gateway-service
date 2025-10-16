0215667b24f6731256ed5441d6330249770bcb04
salikhanov <SAlikhanov@oms.beeline.ru>
2025-10-16 19:24:09 +0600

    SFDM-2841 Убрал из черного списка api/v1/tech-capabilities, api/v1/business-capability

--- src/main/java/ru/beeline/fdmgateway/filter/ValidateTokenFilter.java
+++ src/main/java/ru/beeline/fdmgateway/filter/ValidateTokenFilter.java
@@ -51,9 +51,7 @@ public class ValidateTokenFilter implements WebFilter {
             "/eauthkey"
     );
     private static final Set<String> BLACK_LIST_PATHS = Set.of(
-            "api/v1/tech-capabilities",
-            "api/v1/service",
-            "api/v1/business-capability"
+            "api/v1/service"
     );
 
     @Autowired
