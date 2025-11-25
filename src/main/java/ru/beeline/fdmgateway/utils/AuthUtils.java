bec758b029ac47014b61051e901e497e6cfd6fda
Konstantin <KYuSamsonov@tum.beeline.ru>
2025-11-25 17:43:14 +0300

    fix bild

--- src/main/java/ru/beeline/fdmgateway/utils/AuthUtils.java
+++ src/main/java/ru/beeline/fdmgateway/utils/AuthUtils.java
@@ -33,6 +33,7 @@ public class AuthUtils {
 
     public static String bytesToHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
+
         for (byte b : bytes) {
             sb.append(String.format("%02x", b));
         }
