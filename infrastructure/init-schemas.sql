684e270754dcfb10531d56b0797e51ca26d9e6c0
salikhanov <SAlikhanov@oms.beeline.ru>
2026-03-11 13:01:29 +0600

    Revert "Создать общий docker-compose"
    This reverts commit d559a39a9b63ef6f2d88cac2f312680c6dfd572e.

--- infrastructure/init-schemas.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS product;
-CREATE SCHEMA IF NOT EXISTS capability;
-CREATE SCHEMA IF NOT EXISTS user_auth;
-CREATE SCHEMA IF NOT EXISTS techradar;
-
-DO $$
-BEGIN
-    RAISE NOTICE 'Schemas created successfully';
-END $$;
